592c0621996499b63586941335fc35bb782150c7
Cheetahq <89744299+Cheetahq@users.noreply.github.com>
2021-08-29 13:00:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(400,500);
+
+  c = createSprite(20,20,10,10)
+}
+
+function draw() 
+{
+  background(0);
+
+  textSize(30)
+  fill("white")
+  text("Hi this is Hamzah" , 100, 100 )
+
+  drawSprites()
+}
+
+
+
+
